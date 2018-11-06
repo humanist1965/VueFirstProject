a4e4679aa0a80fb989af7333eb96639a2b64ab35
mkersh <kershaw.mark@gmail.com>
2018-11-06 15:20:57 +0000

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
+  <div>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page XXXXX</h1>
+    <p> When I have auto save on. Let's see if its a problem </p>
+  </div>
+  <div class="body">
+    This is another DIV
+  </div>
   </div>
 </template>
