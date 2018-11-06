99d6a2a57f4d1846a05754dec68d44fbf297c409
mkersh <kershaw.mark@gmail.com>
2018-11-06 18:00:46 +0000

    updates

--- src/views/MKTest.vue
+++ src/views/MKTest.vue
@@ -1,8 +1,23 @@
+
+
 <template>
   <div>
-  <div class="MKTest">
+  <div class="MKTest enjoy-css">
     <h1>MKTest</h1>
     <p> If you see this in the browser then I have successfully created a new view </p>
   </div>
   </div>
 </template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.MKTest {
+   background-color: blue;
+   color: antiquewhite; 
+}
+
+</style>
+
+
+
+
